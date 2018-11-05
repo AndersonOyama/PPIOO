3afeff6b940d89c71dc3119ff6b5fa52f82f92be
AndersonOyama <AndersonOyama@users.noreply.github.com>
2018-11-05 14:29:15 -0200

    start

---
+++ src/colecoes/ColecoesServico.java
@@ -0,0 +1,7 @@
+package colecoes;
+
+// Cada método desta interface é responsável por uma funcionalidade do programa.
+// Os métodos declarados nesta interface devem ser implementados na classe
+// LocalColecoesServico.
+public interface ColecoesServico {
+}
---
+++ src/colecoes/LocalColecoesServico.java
@@ -0,0 +1,13 @@
+package colecoes;
+
+// Uma implementação de ColecoesServico que armazena os dados e executa as
+// operações localmente. Todas as operações de armazenamento e recuperação de
+// dados são feitas através da instância dao.
+public class LocalColecoesServico implements ColecoesServico {
+
+    private final ColecoesDao dao;
+
+    public LocalColecoesServico(ColecoesDao dao) {
+        this.dao = dao;
+    }
+}
